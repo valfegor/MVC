f72b49beb8b41e0a383a8896f952897f7cb72c5c
Anthony Ahumada <locopala99@gmail.com>
2021-07-30 09:01:45 +0200

    more

---
+++ app/Http/Controllers/ProductoAPIController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Producto;
+use Illuminate\Http\Request;
+
+class ProductoAPIController extends Controller
+{
+    ////Entrega una consulta , listamos en un apartado HTML todos los elementos o productos
+    public function index(){
+        $productos = Producto::get();
+        
+        if (!$products) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, Not Acceptable.',
+            ], 406);
+        } else {
+            return response()->json([
+                'success' => true,
+                'products' => $products,
+            ], 200);
+        }
+
+
+
+    }
+    public function store(Request $request)
+    {
+        $producto = new Producto();
+        $producto->name = $request->name;
+        $producto->description = $request->description;
+        $producto->brand = $request->brand;
+        $producto->category = $request->category;
+        $producto->price = $request->price;
+
+        $producto->save();
+        return redirect('producto');
+
+    }
+}
